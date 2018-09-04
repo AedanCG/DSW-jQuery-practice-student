268955c72ea17ea8e058a3015543f7682659519d
AedanCG <aedangarnand@gmail.com>
2018-09-04 10:42:23 -0700

    Edited img in css

--- testquery.js
+++ testquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("h1").click(function(){
 		//$("p").toggle();
-		$("p").css(("color":"red", "background-color":"yellow"));
+		$("p").css({"color":"red", "background-color":"yellow"});
 	});
 	$("#p1").click(function(){
 		$(".p234").toggle();
