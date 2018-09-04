755e42c3682b903783e6bfd6ecba1104c9b0e72a
AedanCG <aedangarnand@gmail.com>
2018-09-04 10:43:29 -0700

    Border for img

--- testquery.js
+++ testquery.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$("h1").click(function(){
 		//$("p").toggle();
 		$("p").css({"color":"red", "background-color":"yellow"});
+		$("p").css({"border":"25px"});
 	});
 	$("#p1").click(function(){
 		$(".p234").toggle();
