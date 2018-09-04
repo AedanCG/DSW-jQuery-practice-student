df148ddee905f3b5dcc2fd47de2426296b183898
AedanCG <aedangarnand@gmail.com>
2018-09-04 10:47:04 -0700

    border

--- testquery.js
+++ testquery.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("h1").click(function(){
 		//$("p").toggle();
 		$("p").css({"color":"red", "background-color":"yellow"});
-		$("img").css({"border":"25px", "border-color":"red"});
+		$("img").css({"border":"2px solid red"});
 	});
 	$("#p1").click(function(){
 		$(".p234").toggle();
