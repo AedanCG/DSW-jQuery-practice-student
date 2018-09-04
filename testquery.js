08855ebad52b6662f48b6cc67dbf5df40044c835
AedanCG <aedangarnand@gmail.com>
2018-09-04 10:29:02 -0700

    Changed jQuery

--- testquery.js
+++ testquery.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+	$("h1").click(function(){
+		//$("p").toggle();
+		$("p").css(("color":"red", "background-color":"yellow"));
+	});
+	$("#p1").click(function(){
+		$(".p234").toggle();
+		$("img").toggle();
+	});
+});
